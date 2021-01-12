027561bbb17433d733106b5cd4fabf313a09a165
Valeriya_Kolobova <KolobovaLera@mail.ru>
2021-01-12 17:55:56 +0300

    refactor: modify functions names and visibility

--- scripts/index.js
+++ scripts/index.js
@@ -32,7 +32,28 @@ function closePopupOverlay(popupOverlay) {
   popupOverlay.classList.remove('popup_opened');
 }
 
+function handleLikeButton(evt) {
+  evt.target.classList.toggle('elements__like-button_active');
+}
+
+function handleDeleteCard(evt) {
+  evt.target.closest('.elements__card').remove();
+}
+
+/*
+function opens the popup with the enlarged existing image and its full description
+*/
+function handlePreviewImage(photo) {
+  openPopupOverlay(imagePopupOverlay);
+  imageCaptionPopup.textContent = photo.name;
+  imagePopup.src = photo.link;
+  imagePopup.alt = `Изображение ${photo.name}`;
+}
+
 function createCard(image) {
+  /*
+  getting access to the card template and its child elements
+  */
   const cardElement = cardTemplate.cloneNode(true);
 
   const likeButton = cardElement.querySelector('.elements__like-button');
@@ -41,30 +62,23 @@ function createCard(image) {
   const cardImage = cardElement.querySelector('.elements__image');
   const cardImageName = cardElement.querySelector('.elements__title');
 
+  /*
+  filling the card's elements with contents
+  */
   cardImageName.textContent = image.name;
   cardImage.src = image.link;
   cardImage.alt = `Изображение ${image.name}`;
 
-  function handleLikeButton(evt) {
-    evt.target.classList.toggle('elements__like-button_active');
-  }
-
-  function handleDeleteCard(evt) {
-    evt.target.closest('.elements__card').remove();
-  }
-
-  function handlePreviewImage(evt) {
-    openPopupOverlay(imagePopupOverlay);
-    imageCaptionPopup.textContent = evt.target.nextElementSibling.firstElementChild.textContent;
-    imagePopup.src = evt.target.src;
-    imagePopup.alt = evt.target.alt;
-  }
-
+  /*
+  adding event handlers to the card's elements
+  */
   likeButton.addEventListener('click', handleLikeButton);
 
   deleteButton.addEventListener('click', handleDeleteCard);
 
-  cardImage.addEventListener('click', handlePreviewImage);
+  cardImage.addEventListener('click', () => {
+    handlePreviewImage(image);
+  });
 
   return cardElement;
 }
@@ -73,37 +87,54 @@ function addCard(parentElem, card) {
   parentElem.prepend(card);
 }
 
+/*
+rendering the initial page: going through every element of the input array with images,
+creating the cards with the images,
+adding the image cards to their parent container
+*/
 initialCards.forEach(item => {
   addCard(cardsList, createCard(item));
 });
 
-function openProfilePopup() {
+/*
+opening the profile popup with the existing values for the user's name and job displayed on the main page
+*/
+function handleProfilePopup() {
   inputName.value = profileName.textContent;
   inputJob.value = profileJob.textContent;
   openPopupOverlay(profilePopupOverlay);
 }
 
-function submitProfileForm(evt) {
+/*
+reading new values of the user's name and job and their rendering on the main page
+*/
+function handleProfileForm(evt) {
   evt.preventDefault();
   profileName.textContent = inputName.value;
   profileJob.textContent = inputJob.value;
   closePopupOverlay(profilePopupOverlay);
 }
 
-function submitNewImagePopupForm(evt) {
+/*
+adding a new image:
+reading the values of the new image's source and title and creating a new image card from them,
+reseting the values of the form's text inputs to their initial values,
+closing the popup
+*/
+function handleNewImagePopupForm(evt) {
   evt.preventDefault();
   addCard(cardsList, createCard({name: newImageName.value, link: newImageLink.value}));
   newImagePopupForm.reset();
   closePopupOverlay(newImagePopupOverlay);
 }
 
-profileEditButton.addEventListener('click', openProfilePopup);
+profileEditButton.addEventListener('click', handleProfilePopup);
 
 profilePopupCloseButton.addEventListener('click', () => {
   closePopupOverlay(profilePopupOverlay);
 });
 
-profilePopupForm.addEventListener('submit', submitProfileForm);
+profilePopupForm.addEventListener('submit', handleProfileForm);
 
 imageAddButton.addEventListener('click', () => {
   openPopupOverlay(newImagePopupOverlay);
@@ -113,7 +144,7 @@ newImagePopupCloseButton.addEventListener('click', () => {
   closePopupOverlay(newImagePopupOverlay);
 });
 
-newImagePopupForm.addEventListener('submit', submitNewImagePopupForm);
+newImagePopupForm.addEventListener('submit', handleNewImagePopupForm);
 
 imagePopupCloseButton.addEventListener('click', () => {
   closePopupOverlay(imagePopupOverlay);
